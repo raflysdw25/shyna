cbbe4fd34aba3a26eacea30700757762e7631301
msadewa <msadewa@fintax.id>
2020-10-28 09:43:28 +0700

    UPDATE Integrasi Product

--- src/components/WomenBannerShyna.vue
+++ src/components/WomenBannerShyna.vue
@@ -1,119 +1,82 @@
 <template>
-    <!-- Women Banner Section Begin -->
-    <section class="women-banner spad">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 mt-5">
-                    <carousel class="product-slider" :items="3" :autoplay="true" :dots="false" :nav="false">
-                        
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/mickey1.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view">
-                                        <router-link to="/product">+ Quick View</router-link>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
-                                <router-link to="/product">
-                                    <h5>Mickey Baggy</h5>  
-                                </router-link>
-                                <div class="product-price">
-                                    $14.00
-                                    <span>$35.00</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-2.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Shoes</div>
-                                <a href="#">
-                                    <h5>Guangzhou sweater</h5>
-                                </a>
-                                <div class="product-price">
-                                    $13.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-3.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Pure Pineapple</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-                        <div class="product-item">
-                            <div class="pi-pic">
-                                <img src="img/products/women-4.jpg" alt="" />
-                                <ul>
-                                    <li class="w-icon active">
-                                        <a href="#"><i class="icon_bag_alt"></i></a>
-                                    </li>
-                                    <li class="quick-view"><a href="#">+ Quick View</a></li>
-                                    <li class="w-icon">
-                                        <a href="#"><i class="fa fa-random"></i></a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div class="pi-text">
-                                <div class="catagory-name">Towel</div>
-                                <a href="#">
-                                    <h5>Converse Shoes</h5>
-                                </a>
-                                <div class="product-price">
-                                    $34.00
-                                </div>
-                            </div>
-                        </div>
-
-                    </carousel>
+  <!-- Women Banner Section Begin -->
+  <section class="women-banner spad">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-12 mt-5" v-if="products.length > 0">
+          <carousel
+            class="product-slider"
+            :items="3"
+            :autoplay="true"
+            :dots="false"
+            :nav="false"
+          >
+            <div
+              class="product-item"
+              v-for="itemProduct in products"
+              :key="itemProduct.id"
+            >
+              <div class="pi-pic">
+                <div v-for="gallery in itemProduct.galleries" :key="gallery.id">
+                  <img
+                    :src="gallery.photo"
+                    v-if="gallery.is_default === 1"
+                    alt=""
+                  />
                 </div>
+                <ul>
+                  <li class="w-icon active">
+                    <a href="#"><i class="icon_bag_alt"></i></a>
+                  </li>
+                  <li class="quick-view">
+                    <router-link to="/product">+ Quick View</router-link>
+                  </li>
+                </ul>
+              </div>
+              <div class="pi-text">
+                <div class="catagory-name">{{ itemProduct.type }}</div>
+                <router-link to="/product">
+                  <h5>{{ itemProduct.name }}</h5>
+                </router-link>
+                <div class="product-price">${{ itemProduct.price }}</div>
+              </div>
             </div>
+          </carousel>
         </div>
-    </section>
-    <!-- Women Banner Section End -->
+        <div class="col-lg-12" v-else>
+          <p>Produk terbaru belum tersedia</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Women Banner Section End -->
 </template>
 
 <script>
-import carousel from 'vue-owl-carousel';
+import carousel from "vue-owl-carousel";
+import axios from "axios";
 
 export default {
-    name: 'WomenBannerShyna',
-    components: {
-        carousel
-    }
-}
+  name: "WomenBannerShyna",
+  components: {
+    carousel,
+  },
+  data() {
+    return {
+      products: {},
+    };
+  },
+  mounted() {
+    axios
+      .get("http://127.0.0.1:8000/api/products")
+      .then((response) => (this.products = response.data.data.data))
+      .catch((err) => console.log(err));
+  },
+};
 </script>
 
 <style>
-    .product-item{
-        margin-left: 25px;
-    }
+.product-item {
+  margin-left: 25px;
+}
 </style>
